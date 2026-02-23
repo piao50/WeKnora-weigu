b72e7cf5bbf88aaf2f660b8ad6a94b1337c36299
Gu Shi <piao50@gmail.com>
2026-02-23 11:25:12 +0800

    add allow host by vscode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-    // 代理配置，用于开发环境
+      allowedHosts: ['hy.3dgu.com'],
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
